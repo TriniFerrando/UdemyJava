0b341299190570a01f5d824b5f21c0b99b55203b
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-02 20:17:04 -0300

    Explicación de etiquetas con el While

--- src/EtiquetasWhile.java
+++ src/EtiquetasWhile.java
@@ -1,5 +1,21 @@
 public class EtiquetasWhile {
     public static void main(String[] args) {
 
+        int contador = 0;
+
+        bucleWhile: // La etiqueta del bucle while
+        while (contador < 10) {
+            System.out.println("Contador: " + contador);
+
+            // Supongamos que queremos saltar al final del bucle si contador es igual a 5
+            if (contador == 5) {
+                System.out.println("Contador es igual a 5, saltando al final del bucle");
+                break bucleWhile; // Salta al final del bucle usando la etiqueta
+            }
+
+            contador++;
+        }
+
+        System.out.println("El bucle while ha finalizado");
     }
 }
