4169d53cb4f2a58584115d857a76a0406cb2794f
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-02 19:49:25 -0300

    Do while - While

--- src/WhileAndDoWhile.java
+++ src/WhileAndDoWhile.java
@@ -1,5 +1,24 @@
+
+// While--> no entra !=  do while--> entra por lo menos una vez
 public class WhileAndDoWhile {
     public static void main(String[] args) {
+
+        int i =0;
+
+        while(i<=5){
+            System.out.println("i = " + i);
+            i++;
+        }
+
+
+        boolean prueba = true;
+        do{
+            if(i==4){
+            System.out.println("Se ejecuto por lo menos una vez");
+            i++;}
+
+        } while(prueba);
+
 
     }
 }
