f45ae5a9a210b582240780f831a7cf43140ed20b
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-02 21:37:07 -0300

    Sentencia Bucle Etiquetas Buscar v0.01

--- src/SentenciaBucleEtiquetasBuscar.java
+++ src/SentenciaBucleEtiquetasBuscar.java
@@ -1,17 +1,27 @@
 public class SentenciaBucleEtiquetasBuscar {
     public static void main(String[] args) {
         String frase = "tres triste tigres tragan trigo en un trigal";
+        String palabra = "trigo";
 
         int largo = frase.length();
+        int maxPalabra=palabra.length();
+
         int cantidad=0;
         char letra = 't';
 
-        for (int i=0; i<largo ;i++){
-            if(frase.charAt(i) != letra){
-                continue;}
+        buscar: //creo la etiqueta
+
+        for (int i=0; i<largo ;i++){ //se recorre toda la frase
+            int k = i;
+            for(int j= 0; j < maxPalabra; j++){
+                if(frase.charAt(k++) != palabra.charAt(j)){
+                    continue buscar;
+                }
+            }
             cantidad++;
         }
-        System.out.println("Hay " + cantidad +" de letras " + letra);
+        System.out.println("Hay " + cantidad +" de letras " + letra + " en la palabra "+ palabra);
+
 
     }
 
