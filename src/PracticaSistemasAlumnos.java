d50eb6de2f2c25d94c161b3428fb1b5af8ed4e5d
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-03 00:46:35 -0300

    Practica sistemas de alumnos

--- src/PracticaSistemasAlumnos.java
+++ src/PracticaSistemasAlumnos.java
@@ -9,10 +9,51 @@ Opcional: si una de las notas ingresadas es 0 debe salirse del ciclo for y mostr
 
  */
 
+import java.util.Scanner;
+
 public class PracticaSistemasAlumnos {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese la nota: ");
+        double nota = scanner.nextDouble();
+        double sum = nota;
+        double sumUnos = nota;
+        double sumMayoresCinco = nota;
+        int cont1 = 0;
+        double sumMenoresCuatro = nota;
+        int cont2 = 0;
 
+        while (nota != 0) {
+            for (int i = 0; i < 19; i++) {
+                if (nota >= 1 && nota <= 7) {
+                    System.out.println("Ingrese la nota: ");
+                    nota = scanner.nextDouble();
+                    sum += nota;
 
+                    if (nota == 1)
+                        sumUnos += nota;
+
+                    if (nota >= 5) {
+                        sumMayoresCinco += nota;
+                        cont1++;
+                    }
+
+                    if (nota < 4) {
+                        sumMenoresCuatro += nota;
+                        cont2++;
+                    }
+                }
+            }
+
+            System.out.println("Cantidad de notas ingresadas son: " + nota);
+            System.out.println("El promedio de todas las notas: " + sum / 6);
+            System.out.println("Notas con valor a 1: " + sumUnos);
+            System.out.println("Promedio de las notas mayores a 5: " + sumMayoresCinco / cont1);
+            System.out.println("Promedio de las notas menores a 4: " + sumMenoresCuatro / cont2);
+
+        }
+        System.out.println("Error.");
     }
+
 }
