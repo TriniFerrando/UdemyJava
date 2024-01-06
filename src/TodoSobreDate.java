4d342930696557f233857aae802eb1819076493a
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-06 12:32:21 -0300

    Java Util Date 0.0

---
+++ src/TodoSobreDate.java
@@ -0,0 +1,9 @@
+import java.util.Date; //importa la biblio de Date
+
+public class TodoSobreDate {
+    public static void main(String[] args) {
+        Date fecha = new Date();  //Declaro el formato Date.
+
+        System.out.println("fecha = " + fecha); //La muestro
+    }
+}
