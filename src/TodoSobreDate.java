23e1dd60afbfee13464560480346a9088f3d2a1c
Trinidad Ferrando <triniferrando19@gmail.com>
2024-01-06 12:56:13 -0300

    Java Util Date 0.1 SimpleDateFormat

--- src/TodoSobreDate.java
+++ src/TodoSobreDate.java
@@ -1,9 +1,14 @@
+import java.text.SimpleDateFormat;
 import java.util.Date; //importa la biblio de Date
 
 public class TodoSobreDate {
     public static void main(String[] args) {
         Date fecha = new Date();  //Declaro el formato Date.
+        System.out.println("fecha = " + fecha); //La muestro: 'Sat Jan 06 12:31:48 ART 2024'
 
-        System.out.println("fecha = " + fecha); //La muestro
-    }
+        SimpleDateFormat df = new SimpleDateFormat("dd-mm-yyyy");
+        String fechaStr = df.format(fecha);
+        System.out.println("fechaStr = " + fechaStr);
+   
+
 }
